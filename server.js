7f23c985be7e4496e39dd40c94799a72f8264149
yenkarii <guilherme.antonio1702@gmail.com>
2026-09-24 13:58:42 -0300

    augh

--- server.js
+++ server.js
@@ -38,11 +38,48 @@ app.get("/partidas/:id", (req, res) => {
     const id = Number(req.params.id);
     const partida = PARTIDAS.find(p => p.id === id);
 
-    if(!partida){
-        return res.status(404).json({mensagem: "Partida não encontrada"})
+    if (!partida) {
+        return res.status(404).json({ mensagem: "Partida não encontrada" })
     }
 
     res.status(200).json(partida);
+});
+
+app.post("/partidas", (req, res) => {
+
+    const { jogo, timeA, timeB } = req.body;
+    if (!jogo || !timeA || !timeB) {
+        return res.status(400).json({
+            mensagem: "Informe jogo, timeA e B"
+        });
+    }
+
+    if (timeA.trim().toLowerCase() === timeB.trim().toLowerCase()) {
+        return res.status(400).json({
+            mensagem: "Os times devem ser diferentes."
+        });
+    }
+
+    // const novoId = PARTIDAS.length > 0
+    //     ? Math.max(...PARTIDAS.map(p => p.id)) + 1 : 1;
+        const novoId = PARTIDAS.length > 0
+        ? PARTIDAS[PARTIDAS.length - 1].id +1 : 1;
+
+    const novaPartida = {
+        id: novoId,
+        jogo: jogo.trim(),
+        timeA: timeA.trim(),
+        timeB: timeB.trim(),
+        pontoA: 0,
+        pontoB: 0,
+        status: "agendada",
+    }
+
+    PARTIDAS.push(novaPartida);
+    res.status(201).json({
+        mensagem: "Partida cadastrada!",
+        partida: novaPartida
+    });
 });
 
 const PORT = 3000;
